7feb4efd7fbaa78c6238a0418d7679a52f34f1e8
CLochstampfor60 <cflochjr@gmail.com>
2023-09-13 11:19:59 -0400

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,28 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+ 
+    static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Exercise 1 STARTS
+
+            //Values
+            int a = 23;
+            int b = 4;
+
+            //Functions
+            Console.WriteLine($"Variable Value a = {a}");
+            Console.WriteLine($"Variable Value b = {b}");
+            Console.WriteLine($"Addition: a + b = {a + b}.");
+            Console.WriteLine($"Subtraction: a - b = {a - b}.");
+            Console.WriteLine($"Multiplication: a * b = {a * b}.");
+            Console.WriteLine($"Division: a / b = {a / b}.");
+            Console.WriteLine($"Modulus: a % b = {a % b}.");
+
         }
+
+ 
     }
 }
+
