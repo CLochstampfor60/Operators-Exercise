bf1edf590e6c05a5306fc7e5b33d0511320c7b81
CLochstampfor60 <cflochjr@gmail.com>
2023-09-13 11:38:53 -0400

    exercise 1 complete

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -9,17 +9,29 @@
             //Exercise 1 STARTS
 
             //Values
-            int a = 23;
+            int x = 23;
+            int y = 4;
+            int a = 17;
             int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
 
             //Functions
+            Console.WriteLine($"Variable Value x = {x}");
+            Console.WriteLine($"Variable Value y = {y}");
             Console.WriteLine($"Variable Value a = {a}");
             Console.WriteLine($"Variable Value b = {b}");
-            Console.WriteLine($"Addition: a + b = {a + b}.");
-            Console.WriteLine($"Subtraction: a - b = {a - b}.");
-            Console.WriteLine($"Multiplication: a * b = {a * b}.");
-            Console.WriteLine($"Division: a / b = {a / b}.");
-            Console.WriteLine($"Modulus: a % b = {a % b}.");
+            Console.WriteLine($"Variable Value quotient = a / b");
+            Console.WriteLine($"Variable Value remainder = a % b\n");
+
+            Console.WriteLine($"Addition: x + y = {x + y}.");
+            Console.WriteLine($"Subtraction: x - y = {x - y}.");
+            Console.WriteLine($"Multiplication: x * y = {x * y}.");
+            Console.WriteLine($"Division: x / y = {x / y}.");
+            Console.WriteLine($"Modulus: x % y = {x % y}.\n");
+
+            Console.WriteLine($"The result of the quotient of a and b = {quotient}");
+            Console.WriteLine($"The result of the remainder of a and b = {remainder}");
 
         }
 
